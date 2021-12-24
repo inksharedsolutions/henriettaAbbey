1440b5a8264ce0abdfb15edc3c78f4d1db522e47
Davie <webdev.inkshared@gmail.com>
2021-12-25 04:14:00 +0800

    Added changes to logo and backgrounds

--- src/components/author.js
+++ src/components/author.js
@@ -3,19 +3,16 @@ import { Link } from 'gatsby'
 import AuthorImg from '../../static/author/dummy.png'
 
 const Author = () => {
+
     return (
         <>
             <section className="upper-main" id="main-upper-author">
                 <div className="grid-two-columns">
 
                     <div className="grid-child" id="author-contents">
-
                         <p data-aos="fade-up" data-aos-duration="2000">
-                       
                         Education. I had the opportunity to observe and assist children learn and play at the Lehman College Early Childhood Center.  I continue to work full time with the New York Police Department as a Senior Police Administrative Aide...
- 
                         </p>
-
                         <h1 className="author-name-tag" >
                             <span></span>
                             <span> Henrietta A Abbey</span>
